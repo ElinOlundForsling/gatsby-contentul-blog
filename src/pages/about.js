6d799fa2a9c43fb29a820e86f6ddcff88a359f31
Elin Ölund Forsling <33157767+ElinOlundForsling@users.noreply.github.com>
2020-11-16 15:06:30 +0100

    about page two-point-zero

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,26 +1,46 @@
 import React from "react"
-import { Link } from "gatsby"
+import { StaticQuery, graphql } from "gatsby"
 import Layout from "../components/layout"
-import SEO from "../components/seo"
-const About = () => {
-  return (
-    <Layout>
-      <SEO title="About" />
-      <div className="blogpost">
-        <h1>About</h1>
-        <img
-          alt="Team"
-          src="https://minutes.co/wp-content/uploads/2019/04/shutterstock_1214730637.png"
-        />
-        <p className="body-text">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem
-          eveniet, perspiciatis laborum odio illum maiores commodi at. Dolores
-          iste fuga rerum omnis reiciendis labore veritatis, ex id neque illum
-          odio?
-        </p>
-        <Link to="/">Back to Home</Link>
-      </div>
-    </Layout>
-  )
-}
+
+const About = () => (
+  <StaticQuery
+    query={graphql`
+      query About {
+        contentfulAbout {
+          title
+          body {
+            body
+          }
+          image {
+            file {
+              url
+            }
+          }
+        }
+      }
+    `}
+    render={({
+      contentfulAbout: {
+        title,
+        body,
+        image: {
+          file: { url },
+        },
+      },
+    }) => (
+      <>
+        <Layout>
+          <div class="wrapper">
+            <img alt="HeroImage" src={url} />
+            <div>
+              <h1 className="title">{title}</h1>
+              <h3>{body.body}</h3>
+            </div>
+          </div>
+        </Layout>
+      </>
+    )}
+  />
+)
+
 export default About
